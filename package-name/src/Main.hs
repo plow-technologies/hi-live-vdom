1dd4c3f863941a23f160b2b06c0521c55ef24294
Scott Murphy <scottmurphy09@gmail.com>
2016-02-14 20:52:09 -0600

    modified main and moduleName to contain a hello wordl primer

--- package-name/src/Main.hs
+++ package-name/src/Main.hs
@@ -1,3 +1,13 @@
-module Main where
 
-main = putStrLn "Hello!"
+module Main where
+import           Control.Concurrent.STM.Notify
+import qualified Data.Sequence                 as S
+import           LiveVDom
+import           $moduleName
+
+main :: IO ()
+main = do
+--  mapM_ addCss cssFiles
+--   mapM_ addJs jsFiles
+  container <- createContainer
+  runDom container (return ()) $$ hello
